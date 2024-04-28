717fe22023071af2c876eb18eaf56a05b0a1bd7a
derekgygax <derekgygax@gmail.com>
2024-04-28 19:51:21 +0545

    SectionHeader put in shared

---
+++ _components/sectionHeader/SectionHeader.tsx
@@ -0,0 +1,26 @@
+
+import classNames from 'classnames';
+
+// styles
+import styles from './SectionHeader.module.scss';
+
+interface SectionHeaderProps {
+  title: string;
+  subTitle?: string;
+  classNameTitle?: string;
+}
+
+export const SectionHeader = ({ title, subTitle, classNameTitle }: SectionHeaderProps) => {
+  return (
+    <div className={styles.sectionHeader}>
+      <div className={styles.titleContainer}>
+        <div className={styles.line}></div>
+        <h2 className={classNames(styles.title, classNameTitle)}>{title}</h2>
+        <div className={styles.line}></div>
+      </div>
+      {subTitle && (
+        <h4 className={styles.subTitle}>{subTitle}</h4>
+      )}
+    </div>
+  )
+}
