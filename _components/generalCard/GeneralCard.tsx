2a61e2ebfbf4a25843604e6c9a7c7c43b42e9575
derekgygax <derekgygax@gmail.com>
2024-04-29 15:51:07 +0545

    GeneralCard can have link button

--- _components/generalCard/GeneralCard.tsx
+++ _components/generalCard/GeneralCard.tsx
@@ -5,6 +5,9 @@ import classNames from 'classnames';
 // layouts
 import { Card } from '@/shared/_layouts/card/Card';
 
+// components
+import { LinkButton } from '../linkButton/LinkButton';
+
 // styles
 import styles from './GeneralCard.module.scss';
 
@@ -12,13 +15,31 @@ interface GeneralCardProps {
   cardClassName?: string;
   description: string;
   title: string;
+  callToActionButton?: {
+    href: string;
+    label: string;
+  };
+  classNameCallToActionButton: string;
+
 }
 
-export const GeneralCard = ({ title, description, cardClassName }: GeneralCardProps) => {
+export const GeneralCard = ({
+  title,
+  description,
+  cardClassName,
+  callToActionButton,
+  classNameCallToActionButton
+}: GeneralCardProps) => {
   return (
     <Card className={classNames(styles.generalCard, "cardBorders", cardClassName)}>
       <h3 className={styles.title}>{title}</h3>
       <p className={styles.description}>{description}</p>
+      {callToActionButton && (
+        <LinkButton
+          className={classNames(styles.link, classNameCallToActionButton)}
+          content={callToActionButton}
+        />
+      )}
     </Card>
   )
 }
