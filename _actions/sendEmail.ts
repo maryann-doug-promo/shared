b5c1b56eea1349a4cac416ff005ec783e9d945ea
derekgygax <derekgygax@gmail.com>
2024-04-30 11:03:48 +0545

    redoing sendEmail.ts cause it wasn't working

--- _actions/sendEmail.ts
+++ _actions/sendEmail.ts
@@ -1,5 +1,8 @@
 "use server"
 
+// NODE MAILER
+const nodemailer = require("nodemailer");
+
 // AWS SES
 import ses from '../configureAWS';
 
@@ -10,14 +13,43 @@ import { ContactType } from '@/shared/types/contact';
 // email builder
 import { getReviewEmail, getContactEmail } from '@/shared/lib/buildEmail';
 
-const TO_EMAIL_ADDRESS: string = process.env.EMAIL_ADDRESS ? process.env.EMAIL_ADDRESS : '';
-const FROM_EMAIL_ADDRESS: string = process.env.DEREKGYGAX_DOMAIN_EMAIL ? process.env.DEREKGYGAX_DOMAIN_EMAIL : '';
+interface SendError extends Error {
+  code?: string;
+}
+
+// TODO YOU STILL CANNOT SET UP EMAILS!!!
+// YOU NEED TO GET A WAY THAT DOES THAT STILL!!!
+// BUT FIRST DEVELOPE THE SITES!!!
+
+
+// NODE MAILER
+// const getTransporter = () => {
+//   // This is the stuff for your private domain
+//   // derekgygax.com
+//   // you have a private email
+//   //  services@derekgygax.com
+//   return nodemailer.createTransport({
+//     host: 'mail.privateemail.com', // The SMTP server host from Namecheap
+//     port: 587, // Port for TLS/STARTTLS
+//     secure: false, // True for 465, false for other ports like 587
+//     auth: {
+//       user: process.env.DEREKGYGAX_DOMAIN_EMAIL,
+//       pass: process.env.DEREKGYGAX_DOMAIN_PASSWORD,
+//     },
+//     tls: {
+//       // Do not fail on invalid certs
+//       rejectUnauthorized: false,
+//     }
+//   });
+// }
 
 const sendEmail = (subject: string, html: string) => {
+
+
   const params = {
     Destination: {
       ToAddresses: [
-        TO_EMAIL_ADDRESS,
+        process.env.EMAIL_RECEIVING ?? '',
       ]
     },
     Message: {
@@ -32,24 +64,41 @@ const sendEmail = (subject: string, html: string) => {
         Data: subject
       }
     },
-    Source: `"${process.env.EMAIL_FROM_NAME}" <${FROM_EMAIL_ADDRESS}>`,
+    Source: `"${process.env.EMAIL_FROM_NAME}" <${process.env.EMAIL_SENDING ?? ''}>`,
   };
 
   ses.sendEmail(params, function (err, data) {
 
     console.log(err);
     console.log(data);
-    if (err) {
-      console.error(err, err.stack);
-      // TODO ADD THIS
-      // res.status(500).json({ error: "Email failed to send." });
-    }
-    // else {
+    // if (err) {
+    //   console.log(err, err.stack);
+    //   res.status(500).json({ error: "Email failed to send." });
+    // } else {
     //   console.log(data);
-    //   // TODO ADD THIS!!
-    //   // res.status(200).json({ success: "Email sent." });
+    //   res.status(200).json({ success: "Email sent." });
     // }
   });
+
+
+
+  // const transporter = getTransporter();
+
+  // transporter.sendMail(
+  //   {
+  //     to: TO_EMAIL_ADDRESS,
+  //     from: `"${process.env.EMAIL_FROM_NAME}" ${FROM_EMAIL_ADDRESS}`,
+  //     subject: subject,
+  //     html: html,
+  //   },
+  //   (error: SendError | null, info: any) => {
+  //     if (error) {
+  //       console.error(error);
+  //     } else {
+  //       console.log('Email sent: ' + info.response);
+  //     }
+  //   }
+  // );
 
 }
 
@@ -69,3 +118,39 @@ export const sendContactEmail = async (newContact: ContactType) => {
 
   sendEmail(subject, emailHtml);
 }
+
+
+// import type { NextApiRequest, NextApiResponse } from 'next';
+
+// export default function handler(req: NextApiRequest, res: NextApiResponse) {
+//   const params = {
+//     Destination: { /* required */
+//       ToAddresses: [
+//         'recipient@example.com', // Replace with recipient address
+//       ]
+//     },
+//     Message: { /* required */
+//       Body: { /* required */
+//         Text: {
+//           Charset: "UTF-8",
+//           Data: "This is the message body in text format."
+//         }
+//       },
+//       Subject: {
+//         Charset: 'UTF-8',
+//         Data: 'Test email'
+//       }
+//     },
+//     Source: 'services@derekggyax.com', /* required */
+//   };
+
+//   ses.sendEmail(params, function (err, data) {
+//     if (err) {
+//       console.log(err, err.stack);
+//       res.status(500).json({ error: "Email failed to send." });
+//     } else {
+//       console.log(data);
+//       res.status(200).json({ success: "Email sent." });
+//     }
+//   });
+// }
